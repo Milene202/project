52135246f07699e2414fd5fcfd7ad6be9e8b8889
Milene Mota <milenemota589@gmail.com>
2026-01-12 19:53:33 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode () {
+const html = document.documentElement
+html.classList.toggle('light')
+    
+
+
+//pegar a tag da img
+    const img = document.querySelector("#profile img")
+
+if(html.classList.contains('light')){
+
+ //se tiver no light mode,adicionar a imagem light
+ img.setAttribute('src','./assets/avatar-light.png')
+ } else {
+// se tiver sem o light mode,manter a imagem normal
+img.setAttribute('src' , './assets/avatar.png')
+ }
+
+}
